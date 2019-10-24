a049c999f1d6af9e56210f2f7dcfe8065471ed02
leja0418 <leja0418@gmail.com>
2019-10-24 13:11:07 +0900

    2019-10-24

--- src/main/java/com/hanrabong/web/cfg/RootConfig.java
+++ src/main/java/com/hanrabong/web/cfg/RootConfig.java
@@ -9,6 +9,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -16,39 +17,29 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages= {"com.hanrabong.web"})
+@Import({
+	MybatisConfig.class, ServletConfig.class
+})
 public class RootConfig {
 	
 	@Bean
 	public DataSource dataSource() {
-/*		HikariConfig hikariConfig = new HikariConfig();
+	HikariConfig hikariConfig = new HikariConfig();
 		hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
 		hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/hanrabong?serverTimezone=UTC");
 		hikariConfig.setUsername("hanrabong");
 		hikariConfig.setPassword("hanrabong");
 		
 		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-*/
+		/*	
 	  DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
 		    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		    dataSource.setUrl("jdbc:mysql://localhost:3306/hanrabong?serverTimezone=UTC");
 		    dataSource.setUsername("hanrabong");
 		    dataSource.setPassword("hanrabong");
-		
+*/		
 		return dataSource;
 	}	
-	
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws Exception {
-	  SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-	  factoryBean.setDataSource(dataSource());
-	  return factoryBean.getObject();
-	}
-	
-	@Bean
-	public SqlSessionTemplate sqlSession() throws Exception {
-	  return new SqlSessionTemplate(sqlSessionFactory());
-	}
-	
 	
 }
